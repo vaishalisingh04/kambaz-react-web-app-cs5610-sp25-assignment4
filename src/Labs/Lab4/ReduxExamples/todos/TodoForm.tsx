fe278ceea8faadab457e7535bd8d7a883e1f042d
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-03-31 12:38:57 -0400

    Update TodoForm.tsx

--- src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
@@ -1,39 +1,42 @@
 import { useSelector, useDispatch } from "react-redux";
 import { addTodo, updateTodo, setTodo } from "./todosReducer";
+
 export default function TodoForm() {
-  const { todo } = useSelector((state: any) => state.todosReducer);
-  const dispatch = useDispatch();
-  return (
-    <li className="list-group-item">
-      <div className="row g-3">
-        <div className="col-auto">
-          <input
-            className="form-control"
-            defaultValue={todo.title}
-            onChange={(e) =>
-              dispatch(setTodo({ ...todo, title: e.target.value }))
-            }
-          />
+    const { todo } = useSelector((state: any) => state.todosReducer);
+    const dispatch = useDispatch();
+
+    const handleUpdate = () => {
+        if (todo.id) {
+            dispatch(updateTodo(todo));
+        }
+    };
+
+    const handleAdd = () => {
+        dispatch(addTodo(todo));
+    };
+
+    return (
+        <div className="todo-form">
+            <input
+                className="todo-input"
+                type="text"
+                placeholder="Enter a todo"
+                value={todo.title}
+                onChange={(e) => dispatch(setTodo({ ...todo, title: e.target.value }))}
+            />
+            <button
+                className="update-btn"
+                onClick={handleUpdate}
+                disabled={!todo.id}
+            >
+                Update
+            </button>
+            <button
+                className="add-btn"
+                onClick={handleAdd}
+            >
+                Add
+            </button>
         </div>
-        <div className="col-auto">
-          <button
-            className="btn btn-warning"
-            onClick={() => dispatch(updateTodo(todo))}
-            id="wd-update-todo-click"
-          >
-            {" "}
-            Update{" "}
-          </button>
-          <button
-            className="btn btn-success"
-            onClick={() => dispatch(addTodo(todo))}
-            id="wd-add-todo-click"
-          >
-            {" "}
-            Add{" "}
-          </button>
-        </div>
-      </div>
-    </li>
-  );
+    );
 }
