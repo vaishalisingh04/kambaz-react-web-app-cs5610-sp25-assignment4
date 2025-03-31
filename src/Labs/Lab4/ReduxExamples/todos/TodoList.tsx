b878acc2bbaa416aebb434dec905fa976b7b1063
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-03-31 12:39:28 -0400

    Update TodoList.tsx

--- src/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
+import "./TodoStyles.css";
+
 export default function TodoList() {
-  const { todos } = useSelector((state: any) => state.todosReducer);
-  return (
-    <div>
-      <h2>Todo List</h2>
-      <ul className="list-group">
-        <TodoForm />
-        {todos.map((todo: any) => (
-          <TodoItem todo={todo} />
-        ))}
-      </ul>
-      <hr />
-    </div>
-  );
+    const { todos } = useSelector((state: any) => state.todosReducer);
+
+    return (
+        <div className="todo-container">
+            <h1 className="todo-title">Todo List</h1>
+            <TodoForm />
+            <ul className="todo-list">
+                {todos.map((todo: { id: string; title: string }) => (
+                    <TodoItem key={todo.id} todo={todo} />
+                ))}
+            </ul>
+        </div>
+    );
 }
