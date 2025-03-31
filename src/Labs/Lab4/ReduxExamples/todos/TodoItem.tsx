e8cfdac0c88bde5327b78a4cc982047315cd7cd6
Vaishali Singh <vaishalisingh9820@gmail.com>
2025-03-31 12:39:13 -0400

    Update TodoItem.tsx

--- src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -1,30 +1,30 @@
 import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
+
 export default function TodoItem({
-  todo,
-}: {
-  todo: { id: string; title: string };
+                                     todo,
+                                 }: {
+    todo: { id: string; title: string };
 }) {
-  const dispatch = useDispatch();
-  return (
-    <li key={todo.id} className="list-group-item">
-      {todo.title}
-      <button
-        className="btn btn-primary"
-        onClick={() => dispatch(setTodo(todo))}
-        id="wd-set-todo-click"
-      >
-        {" "}
-        Edit{" "}
-      </button>
-      <button
-        className="btn btn-danger"
-        onClick={() => dispatch(deleteTodo(todo.id))}
-        id="wd-delete-todo-click"
-      >
-        {" "}
-        Delete{" "}
-      </button>
-    </li>
-  );
+    const dispatch = useDispatch();
+
+    return (
+        <li className="todo-item">
+            <span className="todo-text">{todo.title}</span>
+            <div className="todo-actions">
+                <button
+                    className="edit-btn"
+                    onClick={() => dispatch(setTodo(todo))}
+                >
+                    Edit
+                </button>
+                <button
+                    className="delete-btn"
+                    onClick={() => dispatch(deleteTodo(todo.id))}
+                >
+                    Delete
+                </button>
+            </div>
+        </li>
+    );
 }
